324276c039b268fb999e8c01d08b50af44555aa6
paul-E404 <paul_engerling@web.de>
2021-05-08 15:53:38 +0200

    Started working on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'personal-website';
+
+  images = ['office1.jpg', 'office2.jpg', 'office3.jpg'];
+  currentImageIndex = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImageIndex++;
+      this.currentImageIndex = this.currentImageIndex % this.images.length;
+      this.showImage = false;
+      setInterval(() => {
+        this.showImage = true;
+      }, 10)
+    }, 10000);
+  }
+
 }
