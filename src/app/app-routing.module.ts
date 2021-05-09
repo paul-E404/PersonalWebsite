cd122698062b4bedff88af35436251f28f710e71
paul-E404 <paul_engerling@web.de>
2021-05-09 20:31:35 +0200

    Added fb picture and overlay-bars-animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullBodyPictureComponent } from './full-body-picture/full-body-picture.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent },
+  { path: '', component: FullBodyPictureComponent }, /* DOES NOT WORK YET */
   { path: 'legal_notice', component: LegalNoticeComponent }
 ];
 
