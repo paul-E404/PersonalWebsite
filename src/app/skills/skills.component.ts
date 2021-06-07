53a2b0e0ffae0ad65a40df577c4f03086554ec51
paul-E404 <paul_engerling@web.de>
2021-06-07 19:42:25 +0200

    Finished design of skills-sector

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skillsNames = ['HTML', 'CSS, SCSS', 'Javascript', 'Typescript, Angular', 'Bootstrap', 'Material Design', 'Rest-APIs', 'Google Firebase', 'SCRUM, Kanban'];
+  skillsPercentage = [90, 90, 80, 40, 60, 50, 60, 40, 40];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setBarWidth(i: number) {
+    let barWidth = {
+      'width': this.skillsPercentage[i] + '%'
+    };
+    return barWidth;
+  }
+
 }
+
